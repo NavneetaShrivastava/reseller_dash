21a270ea4c13c6e1ecffa92e3bc794085b331aa0
Navneeta <navneeta@ViewQwests-iMac-2.local>
2019-06-12 14:57:30 +0800

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Reseller Dashboard
                 </div>
 
                 <div class="links">
